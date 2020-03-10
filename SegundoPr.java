7f0ff91b7ada68697db0895da29422d21ea0591d
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-03-10 16:22:12 -0300

    Add files via upload

---
+++ SegundoPr.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SegundoPr {
+	
+	public static void main (String [] args) {
+		Scanner entrada = new Scanner (System.in);
+		int idade;
+		
+		System.out.println ("Digite sua idade: "); // Saida de dados
+		idade = entrada.nextInt(); // Entrada de dados
+	}
+}
