993782414458da686c07c4298ce61c598a45b6ff
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-03-12 19:12:36 -0300

    ex 02 pronto TesteDeVelocidade

--- TesteDeVelocidade.java
+++ TesteDeVelocidade.java
@@ -7,26 +7,30 @@ public class TesteDeVelocidade {
  
         System.out.println("Teste de Velocidade");
         System.out.println("Qual a velocidade maxima permitida na via trafegada ?");
- 
-        int velocidade = entrada.nextInt();
+        int velocidadeMaxima = entrada.nextInt();  // velocidade permitida
  
         System.out.println("Seu veiculo eh carro de passeio digite [1] se for onibus digite [2] ");
         int numero = entrada.nextInt();
+       
+        System.out.println("Qual foi a velocidade que voce passou ? "); // velocidade registrada
+        int velocidadeVeiculo = entrada.nextInt();
+       
+       
         // 1 = velocidade * 10 / 100
         // 2 = velocidade * 5 /  100
  
         if (numero == 1) {
             System.out.println("Carro de Passeio");
-            int x = velocidade * 10 / 100;
+            int x = velocidadeMaxima * 10 / 100;
  
-            if (velocidade + x < velocidade) {
+            if (velocidadeMaxima + x > velocidadeVeiculo) {
                 System.out.println("Multado");
             }
         } else if (numero == 2) {
             System.out.println("Onibus");
-            int y = velocidade * 5 / 100;
+            int y = velocidadeMaxima * 5 / 100;
  
-            if (velocidade + y < velocidade) {
+            if (velocidadeMaxima + y > velocidadeVeiculo) {
                 System.out.println("Multado");
             }
         }
