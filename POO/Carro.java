89d34211e0998503ab1e2757542a07b2f5f98215
delvecch1o <61480650+delvecch1o@users.noreply.github.com>
2020-04-01 17:17:20 -0300

    atributos

---
+++ POO/Carro.java
@@ -0,0 +1,18 @@
+package teste;
+
+public class Carro {
+	
+	String fabricante;       // nao tem nada ou seja null vazio
+	String modelo; 		    // nao tem nada ou seja null
+	String cor;		        // nao tem nada ou seja null
+	int anoDeFabricacao;	// nao tem nada ou seja = 0
+}
+
+/*
+dentro da minha classe CARRO estao os atributos desse carro ,
+o fabricante 
+modelo,
+cor,
+ano de fabricacao..
+
+*/
